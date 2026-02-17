56e853f3f6491521c9a2708c6456dd6e836cab08
Monika K <monika@example.com>
2026-02-17 20:24:09 +0530

    backend commit

--- src/main/java/com/example/medicalbackendorg/controller/RecordController.java
+++ src/main/java/com/example/medicalbackendorg/controller/RecordController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/records")
-@CrossOrigin(origins = "http://localhost:3000") // allow React frontend
+@CrossOrigin(origins = "*") // allow React frontend
 public class RecordController {
 
     private final RecordRepository repository;
